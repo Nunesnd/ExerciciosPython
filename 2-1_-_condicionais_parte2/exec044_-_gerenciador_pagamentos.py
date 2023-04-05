4ab7291c7e9f7169440cd18f9c44a3ac850b5b91
Diego Nunes <nunesdiegond@gmail.com>
2023-04-05 16:59:24 -0300

    feito sistema de gerenciamento de pagamentos

--- 2-1_-_condicionais_parte2/exec044_-_gerenciador_pagamentos.py
+++ 2-1_-_condicionais_parte2/exec044_-_gerenciador_pagamentos.py
@@ -5,3 +5,33 @@ calcule o valor a ser pago em um produto considerando, seu preço normal e condi
     - em até 2X no cartão crédito - preço normal
     - 3X ou mais no crédito - add 20% juros
 """
+
+valor = float(input("Valor a ser pago: "))
+
+cond_pag = int(input('Qual a forma de pagamento? \n1 - A vista \n2 - A prazo\n'))
+
+if cond_pag == 1:
+    pag_vista = int(input("Pagamento será:\n1 - Dinheiro/Pix\n2 - Cartão débito\n"))
+    if(pag_vista == 1):
+        desconto = valor * 0.9
+        print(f'Pagamento a vista no pix ou dinheiro, com desconto de 10% fica R${desconto:.2f}')
+    elif(pag_vista == 2):
+        desconto = valor * 0.95
+        print(f'Pagamento no débito, com desconto de 5% fica R${desconto:.2f}')
+    elif (cond_pag != 1) and (cond_pag != 2):
+        print('Por favor informe uma opção correta!')
+elif cond_pag == 2:
+    print("A prazo")
+    prazo = int(input("Em quantas vezes? "))
+    if prazo <= 2:
+        parcela = valor / prazo
+        print(f'Total de pagamento de {prazo}X no valor de R${parcela:.2f} por parcela.\nTotalizando o valor de R${valor:.2f}')
+    else:
+        valor_juros = valor*1.2
+        parcela = (valor_juros) / prazo
+        print(f'Total de pagamento de {prazo}X no valor de R${parcela:.2f} por parcela.\nTotalizando o valor de R${valor_juros:.2f}')
+
+elif (cond_pag != 1) and (cond_pag != 2):
+    print("Opção errada")
+
+print('Volte Sempre')
